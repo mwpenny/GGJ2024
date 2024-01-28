7e9e2d5377e251d7a060c5ca8c7a4f8cc11c1309
Matt Penny <mwpenny@users.noreply.github.com>
2024-01-27 19:21:58 -0500

    Hook up player indicators

---
+++ scripts/player_indicator.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+var ray = null
+
+func _ready():
+	ray = get_node("Ray")
+
+func _process(delta):
+	var collision = ray.get_collision_point()
+	if collision:
+		global_position.y = collision.y
+	else:
+		global_position.y = get_parent().global_position.y
